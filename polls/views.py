58fd0cfa453954e0dfa0711b15672853a655f497
karl <northrupkarl@gmail>
2020-09-28 02:58:09 -0500

    generic views

--- polls/views.py
+++ polls/views.py
@@ -10,26 +10,26 @@ from django.shortcuts import render
 # no longer need to import loader and HttpResponse
 # (you’ll want to keep HttpResponse if you still have the stub methods for detail, results, and vote).
 
-def index(request):
-
-    # #  page’s design is hard-coded in the view.
-    # #  If you want to change the way the page looks, you’ll have to edit this Python code
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-
-    # # uses template, hhtpfesponse etc
-    # #  loads the template called polls/index.html and passes it a context.
-    # #   The context is a dictionary mapping template variable names to Python objects.
-    # template = loader.get_template('polls/index.html')
-    # context = {
-    #     'latest_question_list': latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-
-    # render shortcut rather than template w/ context, httpresp etc
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#
+#     # #  page’s design is hard-coded in the view.
+#     # #  If you want to change the way the page looks, you’ll have to edit this Python code
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+#
+#     # # uses template, hhtpfesponse etc
+#     # #  loads the template called polls/index.html and passes it a context.
+#     # #   The context is a dictionary mapping template variable names to Python objects.
+#     # template = loader.get_template('polls/index.html')
+#     # context = {
+#     #     'latest_question_list': latest_question_list,
+#     # }
+#     # return HttpResponse(template.render(context, request))
+#
+#     # render shortcut rather than template w/ context, httpresp etc
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
 
 
@@ -46,17 +46,17 @@ def index(request):
 #     return render(request, 'polls/detail.html', {'question': question})
 
 from django.shortcuts import get_object_or_404  #shortvut
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
 # def results(request, question_id):
 #     response = "You're looking at the results of question %s."
 #     return HttpResponse(response % question_id)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 
 # def vote(request, question_id):
@@ -103,5 +103,28 @@ The code for our vote() view does have a small problem.
 This is called a race condition. If you are interested, you can read
  Avoiding race conditions using F() to learn how you can solve this issue.
 '''
+
+
+### updaste for generic views
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+
 
 
